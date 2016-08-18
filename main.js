67bda1f7f7584e7fd3cb11fd404c0d7a5310b287
Stephen Langdon <stphnlngdncoding@gmail.com>
2016-08-17 22:34:26 -0700

    so tired, not sure what changed, just commited to be safe

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
+//videos array to be populated
 var videos;
 var rssString = 'https://www.reddit.com/r/videos.rss';
+
+function getRandomVideoString(videos){
+	var videoUrls = videos;
+	var randomVideoIndex = Math.floor(Math.random() * (videoUrls.length - 1));
+	return videoUrls[randomVideoIndex];
+}
+
+function getRandomVideoUrl(videos){
+	var randomVideoString = getRandomVideoString(videos);
+	return 'https://www.youtube.com/embed/' + randomVideoString + '?autoplay=1'
+}
+
+function createVideoStrings(data) {
+	var contentStrings = [];
+	var $entries = $(data).find('entry');
+	for (var entry of $entries) {
+		var inner = $.parseHTML(entry.innerHTML)[2].textContent;
+		var yurlReg = inner.match(/v=([a-zA-Z0-9\_\-]+)&?/);
+		if (yurlReg !== null) {
+			contentStrings.push(yurlReg[1]);
+		}
+	}
+	return contentStrings;
+}
+
+
+
 $.ajax({
   type: 'GET',
   url: rssString,
@@ -24,40 +52,8 @@ $.ajax({
     // this function will still fire, but there won't be any additional
     // information about the error.
   }
-}).done(function(){
-	console.log("videos in .done", videos);
+}).done(function(test){
+	console.log(test);
 	document.getElementById('ytplayer').src = getRandomVideoUrl(videos);
 });
-
-
-function getRandomVideoString(videos){
-	var videoUrls = videos;
-	var randomVideoIndex = Math.floor(Math.random() * (videoUrls.length - 1));
-	return videoUrls[randomVideoIndex];
-}
-
-function getRandomVideoUrl(videos){
-	console.log("videos in getRandom", videos)
-	var randomVideoString = getRandomVideoString(videos);
-	return 'https://www.youtube.com/embed/' + randomVideoString + '?autoplay=1'
-}
-
-function createVideoStrings(data) {
-	var contentStrings = [];
-	// console.log(data)
-	var $entries = $(data).find('entry');
-	// console.log($entries);
-	for (var entry of $entries) {
-		// var parsed = $.parseXML(entry.innerText);
-		// console.log(parsed);
-		var inner = $.parseHTML(entry.innerHTML)[2].textContent;
-		var yurlReg = inner.match(/v=([a-zA-Z0-9\_\-]+)&?/);
-		if (yurlReg !== null) {
-			contentStrings.push(yurlReg[1]);
-		}
-		// var yurl = yurlReg[1];
-		// contentStrings.push(yurl);
-	}
-	return contentStrings;
-}
 
