e2be78a56308bf115787f4f6a979b2950d4211a3
Stephen Langdon <stphnlngdncoding@gmail.com>
2016-08-18 09:47:49 -0700

    added local storage stuff

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
 var feeds = ['https://www.reddit.com/r/youtubehaiku/.rss', 'https://www.reddit.com/r/woahtube.rss', 'https://www.reddit.com/r/trailers.rss', 'https://www.reddit.com/r/Prematurecelebration.rss', 'https://www.reddit.com/r/SoundsLikeMusic.rss', 'https://www.reddit.com/r/videos.rss'];
 var videos = [];
-//$.ajax({
-//  type: 'GET',
-//  url: rssString,
-//  contentType: 'text/plain',
-//
-//  xhrFields: {
-//    withCredentials: false
-//  },
-//
-//  headers: {},
-//
-//  success: function (data) {
-//    videos = createVideoStrings(data);
-//    // console.log("videos", videos);
-//
-//  },
-//
-//  error: function () {
-//    // Here's where you handle an error response.
-//    // Note that if the error was due to a CORS issue,
-//    // this function will still fire, but there won't be any additional
-//    // information about the error.
-//  }
-//}).done(function () {
-//  console.log("videos in .done", videos);
-//  document.getElementById('ytplayer').src = getRandomVideoUrl(videos);
-//});
+
+//check to see if there are videos in local storage
+chrome.storage.local.get('videos', function(data){
+	if (data['videos']) {
+		console.log('storage data hit');
+		document.getElementById('ytplayer').src = getRandomVideoUrl(data['videos']);
+	} else {
+		$.when(getLinkArray(feeds[0]),
+		      getLinkArray(feeds[1]), 
+ 		      getLinkArray(feeds[2]), 
+		      getLinkArray(feeds[3]),
+		 	    getLinkArray(feeds[4]),
+		  	  getLinkArray(feeds[5]))
+		.done(function(){
+			document.getElementById('ytplayer').src = getRandomVideoUrl(videos);
+			chrome.storage.local.set({'videos': videos}, function(){
+				console.log("the videos were saved");
+			});
+		})
+	}
+});
+
+// if (sessionStorage.videos) {
+// 	console.log("Session storage hit");
+// 	var storedVideoArray = sessionStorage.videos.split(",");
+// 	document.getElementById('ytplayer').src = getRandomVideoUrl(storedVideoArray);
+// } else {
+// 	$.when(getLinkArray(feeds[0]),
+// 		   getLinkArray(feeds[1]), 
+//  		   getLinkArray(feeds[2]), 
+// 		   getLinkArray(feeds[3]),
+// 		   getLinkArray(feeds[4]),
+// 		   getLinkArray(feeds[5]))
+// 		.done(function(){
+// 			document.getElementById('ytplayer').src = getRandomVideoUrl(videos);
+// 			sessionStorage.videos = videos;
+// 		})
+// }
 
 
 function getRandomVideoString(videos) {
@@ -36,7 +47,6 @@ function getRandomVideoString(videos) {
 }
 
 function getRandomVideoUrl(videos) {
-  console.log("videos in getRandom", videos)
   var randomVideoString = getRandomVideoString(videos);
   return 'https://www.youtube.com/embed/' + randomVideoString + '?autoplay=1'
 }
@@ -53,16 +63,6 @@ function createVideoStrings(data) {
   }
   return contentStrings;
 }
-
-$.when(getLinkArray(feeds[0]),
-	   getLinkArray(feeds[1]), 
-	   getLinkArray(feeds[2]), 
-	   getLinkArray(feeds[3]),
-	   getLinkArray(feeds[4]),
-	   getLinkArray(feeds[5]))
-	.done(function(){
-		document.getElementById('ytplayer').src = getRandomVideoUrl(videos);
-	})
 
 function getLinkArray(feed){
 	return $.ajax({
@@ -86,3 +86,20 @@ function getLinkArray(feed){
 	  });
 }
 
+// $.when(getLinkArray(feeds[0]),
+// 		   getLinkArray(feeds[1]), 
+//  		   getLinkArray(feeds[2]), 
+// 		   getLinkArray(feeds[3]),
+// 		   getLinkArray(feeds[4]),
+// 		   getLinkArray(feeds[5]))
+// 		.done(function(){
+// 			document.getElementById('ytplayer').src = getRandomVideoUrl(videos);
+// 			chrome.storage.local.set({'videos': videos}, function(){
+// 				console.log("the videos were saved");
+// 				});
+// 			// sessionStorage.videos = videos;
+// 			// console.log(sessionStorage);
+// 			// chrome.storage.local.get('videos', function(data){
+// 			// 		console.log("got videos", data);
+// 			// });
+// 		})
